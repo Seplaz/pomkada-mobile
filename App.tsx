975c860220f45e51bd35de0e0fc8284257606925
Mikhail Karasev <seplaz38@yandex.ru>
2025-09-25 13:57:55 +0800

    Minimal working functionality

--- App.tsx
+++ App.tsx
@@ -1,25 +1,40 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-import MapView from 'react-native-maps';
+import React from "react";
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, View, Text, Button } from "react-native";
+import Map from "./components/Map.web";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <StatusBar style='auto' />
-      <MapView style={styles.map} />
+      <StatusBar style="auto" />
+      <View style={styles.map}>
+        <Map latitude={52.289588} longitude={104.280606} />
+      </View>
+      <View style={styles.menu}>
+        <Text style={styles.menuTitle}>Отслеживание прогулки</Text>
+      </View>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
+    position: "relative",
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    justifyContent: "flex-end",
   },
   map: {
-    width: '100%',
-    height: '100%',
+    flex: 2,
+  },
+  menu: {
+    flex: 1,
+    justifyContent: "flex-start",
+    alignItems: "center",
+    backgroundColor: "#ffffff",
+    padding: 20,
+  },
+  menuTitle: {
+    fontSize: 24,
+    fontWeight: "400",
   },
 });
